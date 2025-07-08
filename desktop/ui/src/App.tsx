eea4190abe06e6c4064a912d3f929d422878af4e
tlt2627 <tristan.rajnath@yahoo.com>
2025-07-08 12:21:04 -0500

    deep dive dashboard

--- desktop/ui/src/App.tsx
+++ desktop/ui/src/App.tsx
@@ -3,6 +3,7 @@ import WelcomePage from './pages/WelcomePage'
 import FeaturesPage from './pages/FeaturesPage'
 import PrivacyPage from './pages/PrivacyPage'
 import HowItWorksPage from './pages/HowItWorksPage'
+import UploadPage from './pages/UploadPage'
 
 // --- Main App Component ---
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/privacy" element={<PrivacyPage />} />
         <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/upload" element={<UploadPage />} />
       </Routes>
     </Router>
   )
